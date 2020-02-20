b98e33c8bd923a4ac2c4ba4655820e02a77cfb94
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-02-20 10:23:31 +0300

    Add model

---
+++ EFCApp/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCApp.Entities
+{
+    class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
